9dc59003c096ce1b877cc2f2ea1b7d5f83e92b45
Martin Jurča <mjurca@centrum.cz>
2018-12-23 18:01:47 +0100

    changed the naive renderer's behavior to allow dynamic content

--- naiveRenderer.ts
+++ naiveRenderer.ts
@@ -1,17 +1,14 @@
 import {ComponentRenderer} from './Component.js'
 
 export const renderToDom: ComponentRenderer<string> = (renderResult: string, mountNode: HTMLElement) => {
-    if (mountNode.shadowRoot) {
-        return
-    }
-
     const template = document.createElement('template')
     template.innerHTML = renderResult
     const ui = document.importNode(template.content, true)
 
-    const shadowRoot = mountNode.attachShadow({
+    const shadowRoot = mountNode.shadowRoot || mountNode.attachShadow({
         mode: 'open',
     })
+    shadowRoot.innerHTML = ''
     while (ui.firstChild) {
         shadowRoot.appendChild(ui)
     }
