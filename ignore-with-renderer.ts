3b1fc800b175d8919a6602b110ed11d049f48b0b
Martin Jurča <mjurca@centrum.cz>
2019-04-19 16:45:50 +0200

    added a alternative entry providing automatic binding to the default renderer and exporting its templating functions

---
+++ ignore-with-renderer.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+import keyed from '../-x-ignore-tpl/keyed.js'
+// @ts-ignore
+import render from '../-x-ignore-tpl/renderer.js'
+// @ts-ignore
+import tpl from '../-x-ignore-tpl/tpl.js'
+import {Component, define, setRenderer} from './ignore.js'
+
+setRenderer(render)
+
+export {
+    Component,
+    define,
+    keyed,
+    tpl,
+}
