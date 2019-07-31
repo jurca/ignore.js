75e4d009924616a68720a5448eff7f25166da6d8
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-07-31 17:01:10 +0200

    implemented and reformatted tests for the runtime

--- __tests__/runtime.ts
+++ __tests__/runtime.ts
@@ -4,73 +4,108 @@ import Component from '../Component.js'
 import * as runtime from '../runtime.js'
 
 describe('runtime', () => {
-    it('should not update components before a renderer has been set', () => {
-        const render = jest.fn()
-        class Foo extends Component {
-            public static is = 'x-foo'
-            public render() {
-                return render()
-            }
+  it('should not update components before a renderer has been set', () => {
+    const render = jest.fn()
+
+    class Foo extends Component {
+      public static is = 'x-foo'
+
+      public render() {
+        return render()
+      }
+    }
+
+    runtime.define(Foo)
+
+    const foo = new Foo() as any
+    foo.isConnected = true
+    foo.connectedCallback()
+
+    expect(render).not.toHaveBeenCalled()
+  })
+
+  describe('setRenderer', () => {
+    it('should update pending components once set', () => {
+      class Foo extends Component {
+        public static is = 'x-foo'
+
+        public render() {
+          return null
         }
-        runtime.define(Foo)
+      }
 
-        const foo = new Foo() as any
-        foo.isConnected = true
-        foo.connectedCallback()
+      runtime.define(Foo)
 
-        expect(render).not.toHaveBeenCalled()
+      const foo = new Foo() as any
+      jest.spyOn(foo, 'render')
+      foo.isConnected = true
+      foo.connectedCallback()
+      expect(foo.render).not.toHaveBeenCalled()
+
+      runtime.setRenderer(jest.fn())
+      expect(foo.render).toHaveBeenCalledTimes(1)
     })
 
-    describe('setRenderer', () => {
-        it('should update pending components once set', () => {
-            class Foo extends Component {
-                public static is = 'x-foo'
-                public render() {
-                    return null
-                }
-            }
-            runtime.define(Foo)
+    afterEach(() => {
+      runtime.setRenderer(null as any)
+    })
+  })
 
-            const foo = new Foo() as any
-            jest.spyOn(foo, 'render')
-            foo.isConnected = true
-            foo.connectedCallback()
-            expect(foo.render).not.toHaveBeenCalled()
+  describe('define', () => {
+    it('should register the component\'s element with the customElements registry', () => {
+      const rand = Math.random()
 
-            runtime.setRenderer(jest.fn())
-            expect(foo.render).toHaveBeenCalledTimes(1)
-        })
+      class Foo extends Component {
+        public static is = `x-foo${rand}`
 
-        afterEach(() => {
-            runtime.setRenderer(null as any)
-        })
+        public render() {
+          return null
+        }
+      }
+
+      runtime.define(Foo)
+      expect((customElements as any).elements[`x-foo${rand}`]).toBe(Foo)
+    })
+  })
+
+  describe('update', () => {
+    it('should batch on-mount renders of multiple new components', () => {
+      class Foo extends Component {
+        public static is = 'x-foo'
+
+        public render() {
+          return null
+        }
+      }
+
+      class Bar extends Component {
+        public static is = 'x-bar'
+
+        public render() {
+          return null
+        }
+      }
+
+      runtime.setRenderer(jest.fn())
+      const foo1 = new Foo() as any
+      const foo2 = new Foo() as any
+      const bar = new Bar() as any
+      jest.spyOn(foo1, 'render')
+      jest.spyOn(foo2, 'render')
+      jest.spyOn(bar, 'render')
+      foo1.isConnected = true
+      foo2.isConnected = true
+      bar.isConnected = true
+      foo1.connectedCallback()
+      expect(foo1.render).not.toHaveBeenCalled()
+      foo2.connectedCallback()
+      expect(foo2.render).not.toHaveBeenCalled()
+      bar.connectedCallback()
+      expect(bar.render).not.toHaveBeenCalled()
     })
 
-    describe('define', () => {
-        it('should register the component\'s element with the customElements registry', () => {
-            const rand = Math.random()
-            class Foo extends Component {
-                public static is = `x-foo${rand}`
-                public render() {
-                    return null
-                }
-            }
-
-            runtime.define(Foo)
-            expect((customElements as any).elements[`x-foo${rand}`]).toBe(Foo)
-        })
+    afterEach(() => {
+      runtime.setRenderer(null as any)
     })
-
-    describe('update', () => {
-        it('should batch on-mount renders of multiple new components', () => {})
-
-        it('should execute simple update cycle for first-time mounted components', () => {
-        })
-
-        it('should execute the full update cycle for repeated updates of components', () => {
-        })
-
-        it('should queue updates of nested components and update them in sequence instead of recursively', () => {
-        })
-    })
+  })
 })
